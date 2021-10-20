f028ff2478895c9a20f4d52088c68ec7ce82292d
Material Eng <no-reply@google.com>
2021-10-20 08:18:41 -0700

    Internal change
    PiperOrigin-RevId: 404543182

--- dart/lib/palettes/core_palette.dart
+++ dart/lib/palettes/core_palette.dart
@@ -23,12 +23,11 @@ class CorePalette {
   /// The number of generated tonal palettes.
   static const size = 5;
 
-  // TODO: consider renaming to primary, secondary, ..., neutralVariant and providing getters
-  final TonalPalette a1;
-  final TonalPalette a2;
-  final TonalPalette a3;
-  final TonalPalette n1;
-  final TonalPalette n2;
+  final TonalPalette primary;
+  final TonalPalette secondary;
+  final TonalPalette tertiary;
+  final TonalPalette neutral;
+  final TonalPalette neutralVariant;
   final TonalPalette error = TonalPalette.of(25, 84);
 
   /// Create a [CorePalette] from a source ARGB color.
@@ -38,11 +37,11 @@ class CorePalette {
   }
 
   CorePalette._(double hue, double chroma)
-      : a1 = TonalPalette.of(hue, math.max(48, chroma)),
-        a2 = TonalPalette.of(hue, 16),
-        a3 = TonalPalette.of(hue + 60, 24),
-        n1 = TonalPalette.of(hue, 4),
-        n2 = TonalPalette.of(hue, 8);
+      : primary = TonalPalette.of(hue, math.max(48, chroma)),
+        secondary = TonalPalette.of(hue, 16),
+        tertiary = TonalPalette.of(hue + 60, 24),
+        neutral = TonalPalette.of(hue, 4),
+        neutralVariant = TonalPalette.of(hue, 8);
 
   /// Create a [CorePalette] from a fixed-size list of ARGB color ints
   /// representing concatenated tonal palettes.
@@ -50,43 +49,55 @@ class CorePalette {
   /// Inverse of [asList].
   CorePalette.fromList(List<int> colors)
       : assert(colors.length == size * TonalPalette.commonSize),
-        a1 = TonalPalette.fromList(
+        primary = TonalPalette.fromList(
             _getPartition(colors, 0, TonalPalette.commonSize)),
-        a2 = TonalPalette.fromList(
+        secondary = TonalPalette.fromList(
             _getPartition(colors, 1, TonalPalette.commonSize)),
-        a3 = TonalPalette.fromList(
+        tertiary = TonalPalette.fromList(
             _getPartition(colors, 2, TonalPalette.commonSize)),
-        n1 = TonalPalette.fromList(
+        neutral = TonalPalette.fromList(
             _getPartition(colors, 3, TonalPalette.commonSize)),
-        n2 = TonalPalette.fromList(
+        neutralVariant = TonalPalette.fromList(
             _getPartition(colors, 4, TonalPalette.commonSize));
 
   /// Returns a list of ARGB color [int]s from concatenated tonal palettes.
   ///
   /// Inverse of [CorePalette.fromList].
-  List<int> asList() =>
-      [...a1.asList, ...a2.asList, ...a3.asList, ...n1.asList, ...n2.asList];
+  List<int> asList() => [
+        ...primary.asList,
+        ...secondary.asList,
+        ...tertiary.asList,
+        ...neutral.asList,
+        ...neutralVariant.asList,
+      ];
 
   @override
   bool operator ==(Object other) =>
       other is CorePalette &&
-      a1 == other.a1 &&
-      a2 == other.a2 &&
-      a3 == other.a3 &&
-      n1 == other.n1 &&
-      n2 == other.n2 &&
+      primary == other.primary &&
+      secondary == other.secondary &&
+      tertiary == other.tertiary &&
+      neutral == other.neutral &&
+      neutralVariant == other.neutralVariant &&
       error == other.error;
 
   @override
-  int get hashCode => Object.hash(a1, a2, a3, n1, n2, error);
+  int get hashCode => Object.hash(
+        primary,
+        secondary,
+        tertiary,
+        neutral,
+        neutralVariant,
+        error,
+      );
 
   @override
   String toString() {
-    return 'A1: $a1\n'
-        'A2: $a2\n'
-        'A3: $a3\n'
-        'N1: $n1\n'
-        'N2: $n2\n'
+    return 'primary: $primary\n'
+        'secondary: $secondary\n'
+        'tertiary: $tertiary\n'
+        'neutral: $neutral\n'
+        'neutralVariant: $neutralVariant\n'
         'error: $error\n';
   }
 }
