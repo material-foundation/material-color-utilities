2038504a3330362dcf0f307a7dde50276bea7b18
Material Eng <no-reply@google.com>
2023-06-28 03:20:58 -0700

    Remove `.js` from import.
    PiperOrigin-RevId: 544005712

--- typescript/dynamiccolor/contrast_curve.ts
+++ typescript/dynamiccolor/contrast_curve.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import * as math from '../utils/math_utils.js.js';
+import * as math from '../utils/math_utils.js';
 
 /**
  * A class containing the contrast curve for a dynamic color on its background.
