7b665423947d346785447b92ac412088ce9c7198
Material Eng <no-reply@google.com>
2023-07-05 06:36:00 -0700

    Minor comment updates in Java library.
    PiperOrigin-RevId: 545652652

--- java/quantize/PointProvider.java
+++ java/quantize/PointProvider.java
@@ -18,9 +18,15 @@ package quantize;
 
 /** An interface to allow use of different color spaces by quantizers. */
 public interface PointProvider {
+  /** The four components in the color space of an sRGB color. */
   public double[] fromInt(int argb);
 
+  /** The ARGB (i.e. hex code) representation of this color. */
   public int toInt(double[] point);
 
+  /**
+   * Squared distance between two colors. Distance is defined by scientific color spaces and
+   * referred to as delta E.
+   */
   public double distance(double[] a, double[] b);
 }
