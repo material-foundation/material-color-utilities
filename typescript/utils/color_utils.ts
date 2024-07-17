61bdf870fa96912abeb34ba2b05725f51bac625d
Material Eng <no-reply@google.com>
2024-07-17 12:42:45 -0700

    Remove Rgba color representation from TS library
    Our goal is to maintain the library equal on every platform. Rgba representation is unique to TS implementation and is not widely used. So we remove it.
    
    Use `alphaFromArgb`, `blueFromArgb`, `greenFromArgb`, `redFromArgb` function instead.
    
    PiperOrigin-RevId: 653328840

--- typescript/utils/color_utils.ts
+++ typescript/utils/color_utils.ts
@@ -277,55 +277,6 @@ export function whitePointD65(): number[] {
   return WHITE_POINT_D65;
 }
 
-/**
- * RGBA component
- * 
- * @param r Red value should be between 0-255
- * @param g Green value should be between 0-255
- * @param b Blue value should be between 0-255
- * @param a Alpha value should be between 0-255
- */
-export interface Rgba {
-  r: number;
-  g: number;
-  b: number;
-  a: number;
-}
-
-/**
- * Return RGBA from a given int32 color
- *
- * @param argb ARGB representation of a int32 color.
- * @return RGBA representation of a int32 color.
- */
-export function rgbaFromArgb(argb: number): Rgba {
-  const r = redFromArgb(argb);
-  const g = greenFromArgb(argb);
-  const b = blueFromArgb(argb);
-  const a = alphaFromArgb(argb);
-  return {r, g, b, a};
-}
-
-/**
- * Return int32 color from a given RGBA component
- * 
- * @param rgba RGBA representation of a int32 color.
- * @returns ARGB representation of a int32 color.
- */
-export function argbFromRgba({r, g, b, a}: Rgba): number {
-  const rValue = clampComponent(r);
-  const gValue = clampComponent(g);
-  const bValue = clampComponent(b);
-  const aValue = clampComponent(a);
-  return (aValue << 24) | (rValue << 16) | (gValue << 8) | bValue;
-}
-
-function clampComponent(value: number) {
-  if (value < 0) return 0;
-  if (value > 255) return 255;
-  return value
-}
-
 function labF(t: number): number {
   const e = 216.0 / 24389.0;
   const kappa = 24389.0 / 27.0;
