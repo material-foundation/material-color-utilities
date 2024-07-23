c8bc9f9588b89bf9e4e8b47c026332003160c82e
Material Eng <no-reply@google.com>
2024-07-23 01:03:30 -0700

    Add unit tests for Scheme Neutral.
    PiperOrigin-RevId: 655062539

--- swift/Tests/MaterialColorUtilitiesTests/SchemeTests/SchemeNeutralTests.swift
+++ swift/Tests/MaterialColorUtilitiesTests/SchemeTests/SchemeNeutralTests.swift
@@ -18,6 +18,151 @@ import XCTest
 @testable import MaterialColorUtilities
 
 final class SchemeNeutralTests: XCTestCase {
+  func testKeyColors() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 0.0)
+
+    XCTAssertEqual(scheme.primaryPaletteKeyColor, 0xff76_7685)
+    XCTAssertEqual(scheme.secondaryPaletteKeyColor, 0xff77_7680)
+    XCTAssertEqual(scheme.tertiaryPaletteKeyColor, 0xff75_758B)
+    XCTAssertEqual(scheme.neutralPaletteKeyColor, 0xff78_7678)
+    XCTAssertEqual(scheme.neutralVariantPaletteKeyColor, 0xff78_7678)
+  }
+
+  func testLightTheme_minContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.primary, 0xff73_7383)
+  }
+
+  func testLightTheme_standardContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.primary, 0xff5d_5d6c)
+  }
+
+  func testLightTheme_maxContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.primary, 0xff2b_2b38)
+  }
+
+  func testLightTheme_minContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xffd9_d7e9)
+  }
+
+  func testLightTheme_standardContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xffe2_e1f3)
+  }
+
+  func testLightTheme_maxContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xff48_4856)
+  }
+
+  func testLightTheme_minContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xff79_7888)
+  }
+
+  func testLightTheme_standardContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xff45_4654)
+  }
+
+  func testLightTheme_maxContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xffff_ffff)
+  }
+
+  func testLightTheme_minContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.surface, 0xfffc_f8fa)
+  }
+
+  func testLightTheme_standardContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.surface, 0xfffc_f8fa)
+  }
+
+  func testLightTheme_maxContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: false, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.surface, 0xfffc_f8fa)
+  }
+
+  func testDarkTheme_minContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.primary, 0xff90_8f9f)
+  }
+
+  func testDarkTheme_standardContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.primary, 0xffc6_c5d6)
+  }
+
+  func testDarkTheme_maxContrast_primary() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.primary, 0xfff0_eeff)
+  }
+
+  func testDarkTheme_minContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xff39_3947)
+  }
+
+  func testDarkTheme_standardContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xff45_4654)
+  }
+
+  func testDarkTheme_maxContrast_primaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.primaryContainer, 0xffc2_c1d2)
+  }
+
+  func testDarkTheme_minContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xff83_8393)
+  }
+
+  func testDarkTheme_standardContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xffe2_e1f3)
+  }
+
+  func testDarkTheme_maxContrast_onPrimaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.onPrimaryContainer, 0xff09_0a16)
+  }
+
+  func testDarkTheme_minContrast_onTertiaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.onTertiaryContainer, 0xff82_8299)
+  }
+
+  func testDarkTheme_standardContrast_onTertiaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.onTertiaryContainer, 0xffe1_e0f9)
+  }
+
+  func testDarkTheme_maxContrast_onTertiaryContainer() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.onTertiaryContainer, 0xff08_0a1b)
+  }
+
+  func testDarkTheme_minContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: -1.0)
+    XCTAssertEqual(scheme.surface, 0xff13_1315)
+  }
+
+  func testDarkTheme_standardContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 0.0)
+    XCTAssertEqual(scheme.surface, 0xff13_1315)
+  }
+
+  func testDarkTheme_maxContrast_surface() {
+    let scheme = SchemeNeutral(sourceColorHct: Hct(0xff00_00ff), isDark: true, contrastLevel: 1.0)
+    XCTAssertEqual(scheme.surface, 0xff13_1315)
+  }
+
   func testSchemeNeutralProvider_returnsIdeniticalSchemeWithSameSourceColor() {
     let sourceColorHct = Hct(0xfffa_2bec)
     let isDark = false
