49e9a62102480c717062d2c67d7c388a40f06fa7
jcc620 <jcronin02@gmail.com>
2025-10-28 22:36:11 +0000

    Use floats for HCT values

--- hctconverter.go
+++ hctconverter.go
@@ -8,21 +8,16 @@ package hctconverter
 */
 import "C"
 
-import "math"
-
 // HCTToRGB converts a HCT-encoded color to an RGB-encoded one.
-func HCTToRGB(hue, chroma, tone int) (red, green, blue int) {
+func HCTToRGB(hue, chroma, tone float64) (red, green, blue int) {
 	cHCT := C.BuildHct(C.double(hue), C.double(chroma), C.double(tone))
 	cRGB := C.HctToRgb(cHCT)
 	return int(cRGB.Red), int(cRGB.Green), int(cRGB.Blue)
 }
 
 // RGBToHCT converts an RGB-encoded color to a HCT-encoded one.
-func RGBToHCT(red, green, blue int) (hue, chroma, tone int) {
+func RGBToHCT(red, green, blue int) (hue, chroma, tone float64) {
 	cRGB := C.BuildRgb(C.int(red), C.int(green), C.int(blue))
 	cHCT := C.RgbToHct(cRGB)
-	hue = int(math.Round(float64(cHCT.Hue)))
-	chroma = int(math.Round(float64(cHCT.Chroma)))
-	tone = int(math.Round(float64(cHCT.Tone)))
-	return
+	return float64(cHCT.Hue), float64(cHCT.Chroma), float64(cHCT.Tone)
 }
