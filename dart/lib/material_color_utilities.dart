1f1939fa821b229821fdad385e556c999315f1c0
Material Eng <no-reply@google.com>
2026-06-16 23:11:54 -0700

    Internal
    PiperOrigin-RevId: 933515373

--- dart/lib/material_color_utilities.dart
+++ dart/lib/material_color_utilities.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 export 'blend/blend.dart';
 export 'contrast/contrast.dart';
 export 'dislike/dislike_analyzer.dart';
